922f3619372dc29ab142441f4e538fe04733a17d
Roman Kalganov <roman_kalganov@epam.com>
2018-08-26 15:32:15 +0300

    Liquid implementation

---
+++ src/main/java/quoters/DeprecationHandlerBeanFactoryPostProcessor.java
@@ -0,0 +1,26 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class DeprecationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor{
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        String[] beanDefinitionNames = configurableListableBeanFactory.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = configurableListableBeanFactory.getBeanDefinition(beanDefinitionName);
+            String beanClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> beanClass = Class.forName(beanClassName);
+                DeprecatedClass annotation = beanClass.getAnnotation(DeprecatedClass.class);
+                if (annotation != null) {
+                    beanDefinition.setBeanClassName(annotation.newImpl().getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
